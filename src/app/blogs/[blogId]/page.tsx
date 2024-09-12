c5c6f386aaa6e9fae5958dee01374fd5c0b49995
Shanto93 <shantoislam7363@gmail.com>
2024-09-12 12:13:59 +0600

    Redux successfully configured

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -8,9 +8,20 @@ type TBlogDetailsParams = {
   };
 };
 
+//First 3 blog will be cache as static and rest blogs will be Server side rendering
+export const generateStaticParams = async () => {
+  const res = await fetch(`http://localhost:5000/blogs`);
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: IBlog) => {
+    blogId: blogs.id;
+  });
+};
+
 const BlogDetailsInfo = async ({ params }: TBlogDetailsParams) => {
   console.log(params);
-  const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`);
+  const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
+    cache: "no-store",
+  });
   const blog = await res.json();
   console.log(blog);
   return (
